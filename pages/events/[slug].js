81b1a2b7ce2b1236a478f767624f7741b1da2042
sitiojcb <info@webjcb.com>
2022-03-20 10:37:02 -0300

    events functionality

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,13 +1,87 @@
-import { useRouter } from "next/router"
-import { Layout } from "@/components/Layout";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import Link from "next/link";
+import Image from "next/image";
+import Layout from "@/components/Layout";
+import EventMap from "@/components/EventMap";
+import { API_URL } from "@/config/index";
+import styles from "@/styles/Event.module.css";
+import { useRouter } from "next/router";
 
-export default function EventPage() {
+export default function EventPage({ evt }) {
   const router = useRouter();
-  console.log(router)
+//console.log(router)
   return (
     <Layout>
-      <h4>my event id</h4>
-    </Layout>
-  )
-}
+      <div className={styles.event}>
+        <span>
+          {new Date(evt.date).toLocaleDateString("en-US")} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        <ToastContainer />
+        {evt.image && (
+          <div className={styles.image}>
+            <Image
+              src={evt.image.formats.medium.url}
+              width={960}
+              height={600}
+            />
+          </div>
+        )}
 
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <EventMap evt={evt} />
+
+        <Link href="/events">
+          <a className={styles.back}>{"<"} Go Back</a>
+        </Link>
+      </div>
+    </Layout>
+  );
+}
+//para generar statics pages se usan getStaticPaths y getStaticProps
+// export async function getStaticPaths() {
+//   const res = await fetch(`${API_URL}/api/events`)
+//   const events = await res.json()
+
+//   const paths = events.map((evt) => ({
+//     params: { slug: evt.slug },
+//   }))
+
+//   return {
+//     paths,
+//     fallback: true,
+//   }
+// }
+
+// export async function getStaticProps({ params: { slug } }) {
+//   const res = await fetch(`${API_URL}/api/events?slug=${slug}`)
+//   const events = await res.json()
+
+//   return {
+//     props: {
+//       evt: events[0],
+//     },
+//     revalidate: 1,
+//   }
+// }
+
+export async function getServerSideProps({ query: { slug } }) {
+  // const res = await fetch(`${API_URL}/api/events?slug=${slug}`);
+  //al pasar a produccion no tengo api/events
+  const res = await fetch(`${API_URL}/events?slug=${slug}`);
+  const events = await res.json();
+
+  return {
+    props: {
+      evt: events[0],
+    },
+  };
+}
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,39 @@
+import Link from "next/link";
+import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
 
-//import styles from '../styles/Home.module.css'
-
-import Head from "next/head";
-import { Layout } from "@/components/Layout";
-
-export default function Home() {
+export default function HomePage({ events }) {
+  //si realizo el console.log aca de events si lo voy a ver en el navegador
+  //console.log(events);
   return (
-    <Layout className=''>
-      <Head>
-        <title>Dj Events</title>
-      </Head>
-      <h1>Home page</h1>
-     
+    <Layout>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  //llamamos api externa
+  // const res = await fetch(`${API_URL}/events`);
+  const res = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=3`);
+  const events = await res.json();
+  //console.log(events);//comenta va a aparecer en nuestra terminal pero no del lado del cliente es decir no en el navegador 
+//comenta daba error porque pedia retorne un objeto
+  return {
+    //props: {events, events.slice(0,3)} //obtiene los ultimos tres
+    props: { events },
+    revalidate: 1,
+  };
 }
